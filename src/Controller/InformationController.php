ed65a7b590469c4ff300332be140540db6961c4a
KietNe14122003 <kietnvbc00049@fpt.edu.vn>
2023-08-16 22:40:41 +0700

    First commit

---
+++ src/Controller/InformationController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class InformationController extends AbstractController
+{
+    #[Route('/about', name: 'app_about')]
+    public function index(): Response
+    {
+        return $this->render('information/about.html.twig', [
+            'controller_name' => 'InformationController',
+        ]);
+    }
+
+    #[Route('/privacy', name: 'app_privacy')]
+    public function privacy(): Response
+    {
+        return $this->render('information/privacy.html.twig', [
+            'controller_name' => 'InformationController',
+        ]);
+    }
+
+    #[Route('/home', name: 'app_home')]
+    public function home(): Response
+    {
+        return $this->render('information/home.html.twig', [
+            'controller_name' => 'InformationController',
+        ]);
+    }
+}
